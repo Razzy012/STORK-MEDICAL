81584aea368e67a4701bd9134791ff0454b78b6f
Razzy <jklanica@email.cz>
2022-02-25 20:15:05 +0100

    first commit

---
+++ src/lib/utils/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnhLFTxV5p7Y84AR3nB_X8aIGaA3qY2XM",
+  authDomain: "stork-medical-e715c.firebaseapp.com",
+  projectId: "stork-medical-e715c",
+  storageBucket: "stork-medical-e715c.appspot.com",
+  messagingSenderId: "253073097203",
+  appId: "1:253073097203:web:a8125ff8772004a5341286"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(app);
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+	import '$lib/utils/firebase'
+	import { doc, setDoc } from "firebase/firestore"; 
+import { firestore } from '$lib/utils/firebase';
+
+	const cityRef = doc(firestore, 'cities', 'BJ');
+	setDoc(cityRef, { capital: true }, { merge: true });
+</script>
+
+
+<slot />
